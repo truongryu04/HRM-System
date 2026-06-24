1cbceeb9a0502b568c7a4d44cc73404566a43eac
Ryu <nguyenductruong200604@gmail.com>
2026-06-24 11:42:15 +0700

    Khởi tạo Backend và cấu hình database

---
+++ backend/src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConfigService } from '@nestjs/config/dist/config.service';
+import databaseConfig from './config/db.config';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = configService.get('database');
+        if (!dbConfig) {
+          throw new Error('Database config is undefined');
+        }
+        return {
+          ...dbConfig,
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
