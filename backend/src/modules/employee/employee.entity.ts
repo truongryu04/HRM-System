f21ce27cabd5c5877b379a8f577f1f32740beb37
Ryu <nguyenductruong200604@gmail.com>
2026-07-07 14:47:40 +0700

    BE: Thêm mối quan hệ giữa employee và leave request

--- backend/src/modules/employee/employee.entity.ts
+++ backend/src/modules/employee/employee.entity.ts
@@ -13,6 +13,7 @@ import { Department } from '../department/department.entity';
 import { Position } from '../position/position.entity';
 import { User } from '../user/user.entity';
 import { WorkShift } from '../work-shifts/work-shifts.entity';
+import { LeaveRequest } from '../leave/leave-request.entity';
 
 export enum EmployeeStatus {
   ACTIVE = 'ACTIVE', // Đang làm việc
@@ -113,4 +114,7 @@ export class Employee {
   @ManyToOne(() => WorkShift)
   @JoinColumn({ name: 'work_shift_id' })
   workShift!: WorkShift;
+
+  @OneToMany(() => LeaveRequest, (leaveRequest) => leaveRequest.employee)
+  leaveRequests?: LeaveRequest[];
 }
